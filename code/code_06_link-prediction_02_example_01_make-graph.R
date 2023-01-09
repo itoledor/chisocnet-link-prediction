b90f4759a5378607fb0a9863750a990dd04855ed
itoledor <nacho.toledo@gmail.com>
2023-01-09 08:58:42 -0300

    update

--- code/code_06_link-prediction_02_example_01_make-graph.R
+++ code/code_06_link-prediction_02_example_01_make-graph.R
@@ -1,3 +1,6 @@
+library(linkprediction)
+library(igraph)
+
 g <- igraph::make_graph(~ A -- C:D:E -- B -- F -- G:H -- I)
 plot(g)
 # Adamic-Adar
@@ -10,3 +13,5 @@ proxfun(g, method="aa", v1 = V(g), value = "matrix")
 proxfun(g, method="aa", value = "matrix")
 proxfun(g, method="aa", value = "edgelist")
 proxfun(g, method="aa")
+
+
--- code/code_06_link-prediction_02_example_02_uw.R
+++ code/code_06_link-prediction_02_example_02_uw.R
@@ -1,37 +1,44 @@
 # Setup -------------------------------------------------------------------
+library(here)
 source(here::here("code", "code_01_setup_01_libraries.R"))
 set.seed(300)
 
 
 # Read data ---------------------------------------------------------------
 graph_uw <- linkprediction::uw
+graph_uw %>% write_rds(here("data", "data_uw.rds"))
+graph_uw <-  read_rds(here("data", "data_uw.rds"))
 
 # Tidy data ---------------------------------------------------------------
-data_uw <- graph_uw %>% as_edgelist() 
-data_uw <- data_uw %>% set_colnames(c("from", "to"))
-data_uw <- data_uw %>% as_tibble()
-data_uw <- data_uw %>% mutate(p1 = E(graph_uw)$p1) 
-data_uw <- data_uw %>% mutate(p2 = E(graph_uw)$p2) 
-data_uw <- data_uw %>% bind_rows(data_uw %>% set_colnames(c("to", "from", "p1", "p2"))) 
-data_uw <- data_uw %>% filter(from > to) 
+data_uw <- graph_uw %>% as_edgelist()                 #pasar a dataframe
+data_uw <- data_uw %>% set_colnames(c("from", "to"))  #generar los nombres
+data_uw <- data_uw %>% as_tibble()                    #pasaro tidiyverse
+data_uw <- data_uw %>% mutate(p1 = E(graph_uw)$p1)    #agregar el atributo p1 al dataframe
+data_uw <- data_uw %>% mutate(p2 = E(graph_uw)$p2)    #agregar el atributo p2 al dataframe 
+data_uw <- data_uw %>% bind_rows(data_uw %>% set_colnames(c("to", "from", "p1", "p2"))) #pegar la base dada vuelta
+data_uw <- data_uw %>% filter(from > to) #filtrar los id mayores que
 
 # Classification of all the dyads in the uw data ---------------------------
-data_uw_full <- tibble(from = V(graph_uw) %>% names, to = V(graph_uw) %>% names) 
+data_uw_full <- tibble(from = V(graph_uw) %>% names, to = V(graph_uw) %>% names) #genera un dataframe con los nodos de la red
 data_uw_full <- data_uw_full %>% complete(from, to) 
 data_uw_full <- data_uw_full %>% filter(from > to) 
-data_uw <- data_uw %>% right_join(data_uw_full) 
-data_uw <- data_uw %>% replace_na(list(p1 = FALSE, p2 = FALSE)) 
+data_uw <- data_uw %>% right_join(data_uw_full) # combinamos la base de todos los posibles nodos con los nodos que existen en la red
+data_uw <- data_uw %>% replace_na(list(p1 = FALSE, p2 = FALSE)) # reemplazamos lo NA por FALSE
 data_uw <- data_uw %>% mutate(test_data = !p1)
 data_uw <- data_uw %>% mutate(new_coauthorship = !p1&p2)
 data_uw %>% count(p1,p2,test_data, new_coauthorship)
 
-# Labelled dataset                              ---------------------------
+# Train partition                              ---------------------------
+data_train <- data_uw %>% filter(!test_data)
+graph_uw_train <- data_train %>% graph_from_data_frame(directed = FALSE, vertices = V(graph_uw) %>% names) 
+
+# Test partitiopn                              ---------------------------
 data_test <- data_uw %>% filter(test_data)
-data_test %>% count(new_coauthorship)
 
 # Link prediction                                ---------------------------
-data_prediction <- linkprediction::proxfun(graph_uw, method = "aa", value = "edgelist")
-data_prediction <- linkprediction::proxfun(graph_uw, method = "rwr", value = "edgelist")
+
+data_prediction <- linkprediction::proxfun(graph_uw_train, method = "rwr", value = "edgelist")
+data_prediction <- linkprediction::proxfun(graph_uw_train, method = "mf", value = "edgelist")
 data_prediction
 data_prediction <- data_prediction %>% as_tibble
 data_prediction <- data_prediction %>% mutate(from = V(graph_uw)[from] %>% names)
